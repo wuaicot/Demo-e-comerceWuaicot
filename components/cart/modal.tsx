7bc3f684f464f370ea5c556d23d8f4c72e8004e8
{username} <wuaicot8@gmail.com>
2025-01-05 00:59:42 -0300

    adding emoticon sad

--- components/cart/modal.tsx
+++ components/cart/modal.tsx
@@ -85,7 +85,7 @@ export default function CartModal() {
               {!cart || cart.lines.length === 0 ? (
                 <div className="mt-20 flex w-full flex-col items-center justify-center overflow-hidden">
                   <ShoppingCartIcon className="h-16" />
-                  <p className="mt-6 text-center text-2xl font-bold">Your cart is empty.</p>
+                  <p className="mt-6 text-center text-2xl font-bold">Your cart is empty 😔</p>
                 </div>
               ) : (
                 <div className="flex h-full flex-col justify-between overflow-hidden p-1">
